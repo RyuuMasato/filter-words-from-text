6b1e0ab8e7c166ceb147019335a52c6e4face508
Jorrit Meulenbeld <Jorrit Meulenbeld>
2017-06-02 17:51:04 +0200

    Add project files.

---
+++ Words that start with the letter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Words_that_start_with_the_letter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
